9b3f3054081dc8209e22b5e6a116aa5d62881de9
Anthony Vydysh <vydysh@sheridancollege.ca>
2025-03-29 12:23:06 -0400

    project created

---
+++ src/main/java/ca/sheridancollege/vydysh/a3_anthony_vydysh/A3AnthonyVydyshApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.vydysh.a3_anthony_vydysh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3AnthonyVydyshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A3AnthonyVydyshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sheridancollege/vydysh/a3_anthony_vydysh/A3AnthonyVydyshApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.vydysh.a3_anthony_vydysh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3AnthonyVydyshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
